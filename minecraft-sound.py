bf0d2b7c10f01eac0c7bd8062e971384f56b9c4d
mstroh76 <m.stroh76@gmail.com>
2020-08-08 00:25:07 +0200

    Music by http://www.nosoapradio.us/ && new public domain sounds

--- minecraft-sound.py
+++ minecraft-sound.py
@@ -1,11 +1,9 @@
-#www.stuffaboutcode.com
+#www.stuffaboutcode.com & GC2.at
 #Raspberry Pi, Minecraft Sound - Add some sound effects to minecraft
+# unchanged music by http://www.nosoapradio.us/, see LICENCE https://gamesounds.xyz/No%20soap%20radio/LICENSE
 
-#import the minecraft.py module from the minecraft directory
-import minecraft.minecraft as minecraft
-#import minecraft block module
-import minecraft.block as block
-#import time, so delays can be used
+from mcpi import minecraft
+from mcpi import block
 import time
 #import pygame to use the mixer to play wav file
 import pygame
@@ -14,23 +12,41 @@ import pygame
 STOPPED = 1
 WALKING = 2
 FALLING = 3
+FLYING  = 4
+SWIMING = 5
 
 if __name__ == "__main__":
+
+
 
     #Connect to minecraft by creating the minecraft object
     # - minecraft needs to be running and in a game
     mc = minecraft.Minecraft.create()
+    mc.postToChat("Minecraft Sound Effects ")
+    mc.postToChat(" by www.stuffaboutcode.com and gc2.at")
 
     #Initialise pygame and the mixer
     pygame.init()
     pygame.mixer.init()
+    
+    #MP3 background music from  http://www.nosoapradio.us/
+    pygame.mixer.music.load("music/DST-BattleLands.mp3")
+    pygame.mixer.music.play(-1)
+    
+    mc.postToChat("music by http://www.nosoapradio.us/")
 
     #load WAVS files
-    soundWalking = pygame.mixer.Sound("sounds/walking.wav")
-    soundJump = pygame.mixer.Sound("sounds/jump.wav")
-    soundFalling = pygame.mixer.Sound("sounds/falling.wav")
-    soundBang = pygame.mixer.Sound("sounds/bang.wav")
-    soundSword = pygame.mixer.Sound("sounds/sword.wav")
+    soundWalking = pygame.mixer.Sound("sounds/walking.wav") #http://soundbible.com/2057-Footsteps-On-Cement.html
+    soundJump = pygame.mixer.Sound("sounds/jump.wav") #http://soundbible.com/1953-Neck-Snap.html
+    soundFalling = pygame.mixer.Sound("sounds/falling.wav") #http://soundbible.com/1247-Wind.html
+    soundLanding = pygame.mixer.Sound("sounds/landing.wav") #http://soundbible.com/1952-Punch-Or-Whack.html
+    soundSword = pygame.mixer.Sound("sounds/sword.wav") #http://soundbible.com/1898-Spin-Jump.html - public domain 
+    soundFlying = pygame.mixer.Sound("sounds/flying.wav") #http://soundbible.com/1036-Propeller.html - public domain 
+    soundSwiming = pygame.mixer.Sound("sounds/swiming.wav") #http://soundbible.com/2032-Water.html - public domai
+    soundSplash = pygame.mixer.Sound("sounds/splash.wav")   #http://soundbible.com/2100-Splash-Rock-In-Lake.html
+
+    
+    mc.postToChat("sound sfx by http://soundbible.com/ (public domain)")
 
     # setup variables
     lastPlayersState = STOPPED
@@ -42,20 +58,26 @@ if __name__ == "__main__":
     #get players position
     lastPlayerPos = mc.player.getPos()
     
-    mc.postToChat("Minecraft Sound Effects - www.stuffaboutcode.com")
     
-    # loop until CTRL C
+    # loop until CTRL+C
     try:
         while(True):
             
             # get players position
             currentPlayerPos = mc.player.getPos()
+            block_beneath = mc.getBlock(lastPlayerPos.x, lastPlayerPos.y-1, lastPlayerPos.z)  # block ID
 
             # has the player moved in either X or Z (are they WALKING?)
             if lastPlayerPos.x != currentPlayerPos.x or lastPlayerPos.z != currentPlayerPos.z:
                 # if player is FALLING they cant be WALKING
+                
                 if playersState != FALLING:
-                    playersState = WALKING
+                    if block_beneath == block.AIR:
+                      playersState = FLYING
+                    elif block_beneath == block.WATER_FLOWING or block_beneath == block.WATER_STATIONARY:                      
+                      playersState = SWIMING
+                    else:
+                      playersState = WALKING
 
             # has the player moved in positive Y (have they jumped?)
             if int(lastPlayerPos.y) < int(currentPlayerPos.y):
@@ -88,6 +110,23 @@ if __name__ == "__main__":
             # player has stopped WALKING
             if lastPlayersState == WALKING and playersState != WALKING:
                 soundWalking.stop()
+                
+             # player has started FLYING
+            if lastPlayersState != FLYING and playersState == FLYING:
+                soundFlying.play(-1)      
+                
+            # player has stopped FLYING
+            if lastPlayersState == FLYING and playersState != FLYING:
+                soundFlying.stop()                              
+
+             # player has started SWIMING
+            if lastPlayersState != SWIMING and playersState == SWIMING:
+                soundSwiming.play(-1)      
+                
+            # player has stopped SWIMING
+            if lastPlayersState == SWIMING and playersState != SWIMING:
+                soundSwiming.stop()                              
+
 
             # if the players state = falling and the distance they have fell is greater than 3
             # player has started FALLING
@@ -100,7 +139,8 @@ if __name__ == "__main__":
             # player has stopped falling
             if lastPlayersState == FALLING and playersState != FALLING and playerFalling == True:
                 soundFalling.stop()
-                soundBang.play()
+                print("landing")
+                soundLanding.play()
                 playerFallDistance = 0
                 playerFalling = False
             
@@ -114,12 +154,14 @@ if __name__ == "__main__":
 
             #Get the block hit events
             blockHits = mc.events.pollBlockHits()
-            # if a block has been hit play sword sound
-            if blockHits:
+            # block has been hit by right-mouse click
+            #if blockHits:
+            for hit in blockHits:
+                print("Hits: " , blockHits)
                 soundSword.play()
             
-            # sleep for a bit
-            time.sleep(0.05)
+            # sleep to meet 20 Hz update
+            time.sleep(0.05) 
 
     except KeyboardInterrupt:
         print("stopped")
